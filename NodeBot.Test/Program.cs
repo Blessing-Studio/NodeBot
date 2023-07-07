e756b7696a68754e8a31e3e3f6237ae6731169b4
JWJUN233233 <1306334428@qq.com>
2023-07-07 10:14:52 +0800

    移除调试代码

--- NodeBot.Test/Program.cs
+++ NodeBot.Test/Program.cs
@@ -22,7 +22,7 @@ namespace NodeBot.Test
             WebhookService webhookService = WebhookService.Instance;
             WebhookService.MessageEvent += (_, e) =>
             {
-                nodeBot.session.SendPrivateMessage(1306334428, new(new CqTextMsg(e.Message)));
+                //nodeBot.session.SendPrivateMessage(1306334428, new(new CqTextMsg(e.Message)));
             };
             nodeBot.RegisterService(webhookService);
             nodeBot.LoadPermission();
