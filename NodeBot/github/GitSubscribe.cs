1dd8025641c6a400d4e50070e81c88decf0e9495
JWJUN233233 <1306334428@qq.com>
2023-07-18 14:29:40 +0800

    Github订阅数据保存

--- NodeBot/github/GitSubscribe.cs
+++ NodeBot/github/GitSubscribe.cs
@@ -13,6 +13,10 @@ namespace NodeBot.github
     public class GitSubscribe : ICommand
     {
         public static List<GitSubscribeInfo> Info = new List<GitSubscribeInfo>();
+        public GitSubscribe()
+        {
+            LoadInfo();
+        }
         public bool Execute(ICommandSender sender, string commandLine)
         {
             return true;
@@ -24,13 +28,23 @@ namespace NodeBot.github
             {
                 string msg = ((CqTextMsg)msgs[0]).Text;
                 string repository = msg.Split(' ')[1];
+                GitSubscribeInfo info =  new(repository, ((GroupQQSender)QQSender).GroupNumber);
+                foreach (var item in Info)
+                {
+                    if(item == info)
+                    {
+                        QQSender.SendMessage($"已经订阅{repository}");
+                        return false;
+                    }
+                }
                 Info.Add(new(repository, ((GroupQQSender)QQSender).GroupNumber));
                 QQSender.SendMessage($"成功订阅{repository}");
             }
-            catch (Exception ex)
+            catch
             {
                 QQSender.SendMessage("检查参数");
             }
+            SaveInfo();
             return true;
         }
 
@@ -57,6 +71,22 @@ namespace NodeBot.github
         public bool IsUserCommand()
         {
             return false;
+        }
+        public void SaveInfo()
+        {
+            if (!File.Exists("GithubSubScribeInfo.json"))
+            {
+                File.Create("GithubSubScribeInfo.json").Close();
+            }
+            File.WriteAllText("GithubSubScribeInfo.json", Newtonsoft.Json.JsonConvert.SerializeObject(Info));
+        }
+        public void LoadInfo()
+        {
+            if (File.Exists("GithubSubScribeInfo.json"))
+            {
+                string json = File.ReadAllText("GithubSubScribeInfo.json");
+                Info = Newtonsoft.Json.JsonConvert.DeserializeObject<List<GitSubscribeInfo>>(json)!;
+            }
         }
     }
 }
