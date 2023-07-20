02121b0997ee1f20247362135e10e58520130c82
JWJUN233233 <1306334428@qq.com>
2023-07-20 19:46:13 +0800

    添加stop命令

---
+++ NodeBot/Command/Stop.cs
@@ -0,0 +1,55 @@
+﻿using ConsoleInteractive;
+using EleCho.GoCqHttpSdk;
+using EleCho.GoCqHttpSdk.Message;
+using NodeBot.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeBot.Command
+{
+    public class Stop : ICommand
+    {
+        public bool Execute(ICommandSender sender, string commandLine)
+        {
+            sender.SendMessage("机器人已停止");
+            Environment.Exit(0);
+            return true;
+        }
+
+        public bool Execute(IQQSender QQSender, CqMessage msgs)
+        {
+            QQSender.SendMessage("机器人已停止");
+            ConsoleWriter.WriteLine($"机器人被{QQSender.GetNumber()}停止");
+            Environment.Exit(0);
+            return true;
+        }
+
+        public int GetDefaultPermission()
+        {
+            return 5;
+        }
+
+        public string GetName()
+        {
+            return "stop";
+        }
+
+        public bool IsConsoleCommand()
+        {
+            return true;
+        }
+
+        public bool IsGroupCommand()
+        {
+            return true;
+        }
+
+        public bool IsUserCommand()
+        {
+            return true;
+        }
+    }
+}
